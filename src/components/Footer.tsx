237bad6e3ed1d6f404ad4717ec8256ffea7ddae5
vikramsamak <vikramsamak02@gmail.com>
2024-09-17 19:56:53 +0530

    Modified Header and footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex p-4 w-full h-12 justify-between items-center">
-      Footer
+    <footer className="flex p-4 w-full h-12 justify-between items-center shadow-inner">
+      <p className="text-sm  ">
+        © {new Date().getFullYear()} RapidTags.Co.In
+      </p>
+      <p className="text-sm">Powered by AI</p>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@ import { ThemeToogle } from "./ThemeToggle";
 
 function Header() {
   return (
-    <header className="flex p-4 w-full h-12 justify-between">
-      Header <ThemeToogle />
+    <header className="flex p-4 w-full h-12 justify-between items-center shadow-md">
+      <h1 className="text-xl font-semibold">
+        RapidTags
+      </h1>
+      <ThemeToogle />
     </header>
   );
 }
