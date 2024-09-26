0528e4584afbbe948ad14512c29527a4953e758e
vikramsamak <vikramsamak02@gmail.com>
2024-09-26 22:50:24 +0530

    Constants for metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,16 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import ReactQueryProvider from "@/contexts/ReactQueryProvider";
+import { METADATA } from "@/constants/Constants";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import ReactQueryProvider from "@/contexts/ReactQueryProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "RapidTags - Best YouTube Tags Generator for SEO Optimization",
-  description:
-    "RapidTags is a powerful YouTube tags generator designed to help creators boost their video SEO and reach a larger audience. Generate relevant and trending tags instantly to optimize your YouTube content for maximum visibility.",
-  keywords: [
-    "RapidTags",
-    "YouTube tags generator",
-    "best YouTube tags",
-    "YouTube SEO tool",
-    "tags for YouTube videos",
-    "video optimization",
-    "increase YouTube views",
-    "YouTube keyword research",
-    "SEO for YouTube",
-    "generate YouTube tags",
-    "trending YouTube tags",
-    "YouTube tags 2023",
-    "boost YouTube SEO",
-    "YouTube marketing tools",
-  ],
+  title: METADATA.title,
+  description: METADATA.description,
+  keywords: METADATA.description,
 };
 
 export default function RootLayout({
