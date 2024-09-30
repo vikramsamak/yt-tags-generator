7d004e76b347345dc9c018f657e96ceef4d3d647
vikramsamak <vikramsamak02@gmail.com>
2024-09-30 20:56:13 +0530

    corrcted opengraph-image.

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,43 +1,27 @@
 import { ImageResponse } from "next/og";
-import { Inter } from "next/font/google";
 import { APP_URL } from "@/constants/Constants";
+import Image from "next/image";
 
 export const runtime = "edge";
 
 // Image metadata
-export const alt = {APP_URL};
+export const alt = APP_URL.replace("https://", "");
 export const size = {
   width: 1200,
   height: 630,
 };
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
 
 export const contentType = "image/png";
 
-// Image generation
-export default async function Image() {
+export default async function Opengraphimage() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 128,
-          fontFamily: inter.style.fontFamily,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {APP_URL}
-      </div>
-    ),
-    {
-      ...size,
-    }
+      <Image
+        src={"/public/icon/icon-512x512.png"}
+        alt={alt}
+        width={size.width}
+        height={size.height}
+      />
+    )
   );
 }
