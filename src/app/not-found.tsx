fc4117d4b6f3057b91c890e0f3b0925c1de8bfb2
vikramsamak <vikramsamak02@gmail.com>
2024-10-02 09:03:59 +0530

    UI chnages in not found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,10 @@
-import Headings from "@/components/Headings";
-
 export default function NotFound() {
   return (
     <main className="flex flex-grow h-full w-full p-4 items-start md:items-center justify-center">
-      <section className="flex gap-8 p-2 w-full">
-        <Headings heading={"Page Not Found!"} />
+      <section className="flex p-2 w-full justify-center">
+        <p className="font-extrabold text-base sm:text-2xl md:text-4xl lg:text-6xl tracking-widest">
+          Page Not Found!
+        </p>
       </section>
     </main>
   );
